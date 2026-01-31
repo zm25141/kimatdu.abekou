9d6468351679d3051ad511f506c55f2f8e46a8ea
zm25141 <zm25141@josai.ac.jp>
2026-01-31 23:01:13 +0900

    Create ex1-1.js

---
+++ ex1-1.js
@@ -0,0 +1,7 @@
+function imgchange1(){
+  document.getElementById('logo').src = './images/R.png';
+}
+
+function imgReset1(){
+  document.getElementById('logo').src = './images/nigirizushi_moriawase.png';
+}
